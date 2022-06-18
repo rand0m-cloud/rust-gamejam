81959a0a04e1620fee967e2a44cb5220f25a3e3f
Addy Bryant <rand0m-cloud@outlook.com>
2022-06-18 13:10:29 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use bevy::{prelude::*, render::camera::ScalingMode, window::PresentMode};
+use bevy_inspector_egui::{WorldInspectorParams, WorldInspectorPlugin};
+
+pub const CLEAR: Color = Color::rgb(0.3, 0.3, 0.3);
+pub const HEIGHT: f32 = 900.0;
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: HEIGHT * RESOLUTION,
+            height: HEIGHT,
+            title: "Bevy Template".to_string(),
+            present_mode: PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .insert_resource(WorldInspectorParams {
+            enabled: false,
+            ..Default::default()
+        })
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(spawn_camera)
+        .add_system(toggle_inspector)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
+
+fn toggle_inspector(
+    input: ResMut<Input<KeyCode>>,
+    mut window_params: ResMut<WorldInspectorParams>,
+) {
+    if input.just_pressed(KeyCode::Grave) {
+        window_params.enabled = !window_params.enabled
+    }
+}
