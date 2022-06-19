2e400c4f2af27c027a46f6733621cbc01da27e0d
Abby Bryant <rand0m-cloud@outlook.com>
2022-06-18 21:08:02 -0400

    fix(enemy): scale movement to delta time (#12)

--- src/enemy.rs
+++ src/enemy.rs
@@ -39,12 +39,13 @@ pub fn enemy_ai(
         Query<&GlobalTransform, With<Player>>,
         Query<(&GlobalTransform, &mut Transform, &MovementStats), With<Enemy>>,
     )>,
+    time: Res<Time>,
 ) {
     let player_translation = query.p0().single().translation;
     for (enemy_global_transform, mut enemy_transform, movement_stats) in query.p1().iter_mut() {
         let dir = player_translation - enemy_global_transform.translation;
         let dir = dir.try_normalize().unwrap_or_default();
 
-        enemy_transform.translation += dir * movement_stats.speed;
+        enemy_transform.translation += dir * movement_stats.speed * time.delta_seconds();
     }
 }
